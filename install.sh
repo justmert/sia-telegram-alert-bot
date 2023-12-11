e5c574d0989fbc8458c96edf676ee5533da07af0
Mert Köklü <kklumert@gmail.com>
2023-12-11 12:38:43 +0300

    updated script for renterd

--- install.sh
+++ install.sh
@@ -3,59 +3,64 @@
 # Improved error handling
 set -euo pipefail
 
-# Server URL
-SERVER_URL="http://www.sia-alert-bot.com:8006"
-
 # Function to display the initial menu and get user choice
-
 echo "⭐ Welcome to Sia Renterd/Hostd Telegram Bot! Which application do you want to register alerts?"
 echo "1. Renterd"
 echo "2. Hostd"
 while true; do
-read -p "Enter your choice (1/2): " app_choice
-case "$app_choice" in
-    1|2) break;;
-    *) echo "Invalid choice. Please select either 1 or 2.";;
-esac
+    read -p "Enter your choice (1/2): " app_choice
+    case "$app_choice" in
+        1|2) break;;
+        *) echo "Invalid choice. Please select either 1 or 2.";;
+    esac
 done
 
 # Set the app_type based on the user's choice
 app_type=$([ "$app_choice" == "1" ] && echo "Renterd" || echo "Hostd")
 
-# Function to check if Sia app is running
-function is_sia_running() {
-  while true; do
-    read -p "Sia $app_type must be running since we require its API to set up webhooks. Is $app_type running? (yes/y): " is_running
-    case "$is_running" in
-      [Yy]*)
-        return 0;;
-      *)
-        echo "Please answer with yes or y."
-    esac
-  done
+# Ask for API URL with default value
+read -p "Enter the Sia API URL [default: localhost:9980]: " api_url
+api_url=${api_url:-localhost:9980}
+
+# Securely get the password before testing if the app is running
+read -s -p "Provide password for $app_type: " PASSWORD
+echo
+
+# Function to check if Sia app is running by accessing its endpoint
+function check_sia_endpoint() {
+    if curl -s -u ":$PASSWORD" --head --request GET "http://$api_url/api/alerts" | grep "200 OK" > /dev/null; then
+        echo "✓ Sia $app_type is running."
+    else
+        echo "Error: Unable to reach Sia $app_type at $api_url. Please check if it is running."
+        exit 1
+    fi
 }
 
 # Check if Sia app is running
-is_sia_running
+check_sia_endpoint
 
 # Get the unique ID from the user
 read -p "Enter your unique id (get from t.me/sia_alert_bot): " unique_id
 
-# Securely get the password
-read -s -p "Provide password for $app_type: " PASSWORD
-echo
-
-# Create the JSON data for the curl request
-# Create the JSON data for the curl request
-json_data='{
-    "module": "alerts",
-    "event": "register",
-    "url": "'"$SERVER_URL/alerts?unique_id=$unique_id&app_type=$app_type"'"
-}'
+# Create the JSON data for the curl request based on app type
+if [ "$app_type" == "Hostd" ]; then
+    json_data='{
+        "scopes": ["alerts.info", "alerts.warning", "alerts.error", "alerts.critical"],
+        "callbackURL": "http://www.sia-alert-bot.com:8006/alerts?unique_id='$unique_id'&app_type='$app_type'"
+    }'
+    api_endpoint="/api/webhooks"
+else
+    json_data='{
+        "module": "alerts",
+        "event": "register",
+        "url": "http://www.sia-alert-bot.com:8006/alerts?unique_id='$unique_id'&app_type='$app_type'"
+    }'
+    api_endpoint="/api/bus/webhooks"
+fi
 
 # Make the curl request and capture the response
-response=$(curl -s -w "%{http_code}" --location "localhost:9980/api/bus/webhooks" \
-  --data "$json_data" -u ":$PASSWORD")
+response=$(curl -s -w "%{http_code}" --location "http://$api_url$api_endpoint" \
+  --data "$json_data" -u ":$PASSWORD" --header 'Content-Type: application/json')
 
 echo $response
 # Extract the HTTP status code from the response
--- test.sh
+++ test.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 
-# Print a message to test the setup
+# Improved error handling
+set -euo pipefail
+
 echo "⭐ Test your setup, and see if it is running successfully"
 
-# Ask the user if the Sia app is running
+# Function to display the initial menu and get user choice
+echo "Which Sia application would you like to test?"
+echo "1. Renterd"
+echo "2. Hostd"
 while true; do
-    read -p "Is the Sia app running? (yes/y): " is_running
-    case "$is_running" in
-        [Yy]|[Yy][Ee][Ss])
-            break
-            ;;
-        *)
-            echo "Please answer with yes or y."
-            ;;
+    read -p "Enter your choice (1/2): " app_choice
+    case "$app_choice" in
+        1|2) break;;
+        *) echo "Invalid choice. Please select either 1 or 2.";;
     esac
 done
 
+# Set the app_type based on the user's choice
+app_type=$([ "$app_choice" == "1" ] && echo "Renterd" || echo "Hostd")
 
-# Ask the user for the password
+# Ask for API URL with default value
+read -p "Enter the Sia API URL [default: localhost:9980]: " api_url
+api_url=${api_url:-localhost:9980}
+
+# Securely get the password before testing if the app is running
 read -s -p "Provide the password: " PASSWORD
 echo # New line for clean output
+
+# Function to check if Sia app is running by accessing its endpoint
+function check_sia_endpoint() {
+    if curl -s -u ":$PASSWORD" --head --request GET "http://$api_url/api/alerts" | grep "200 OK" > /dev/null; then
+        echo "Sia $app_type is running."
+    else
+        echo "Error: Unable to reach Sia $app_type at $api_url. Please check if it is running."
+        exit 1
+    fi
+}
+
+# Check if Sia app is running
+check_sia_endpoint
 
 # Create a JSON payload for registering an alert
 alert_payload='{
@@ -28,12 +48,15 @@ alert_payload='{
     "message": "Testing integration",
     "data": {
         "detail": "You are good to go!"
-        },
+    },
     "timestamp": "2023-08-30T12:20:49.611086295Z"
 }'
 
+# Set the appropriate API endpoint based on the app type
+api_endpoint=$([ "$app_type" == "Renterd" ] && echo "/api/bus/alerts/register" || echo "/api/alerts/register")
+
 # Make a curl request to register an alert
-response=$(curl -s -w "\n%{http_code}" --location "http://localhost:9980/api/bus/alerts/register" \
+response=$(curl -s -w "\n%{http_code}" --location "http://$api_url$api_endpoint" \
 --header "Content-Type: application/json" \
 --request POST \
 --data "$alert_payload" -u ":$PASSWORD")
